c7f5fcaea13e314944a2ea672c2c79ceb700aa51
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-07-07 02:22:45 -0300

    correction for deploy #4

--- components/PoliticasPrivacidade.js
+++ components/PoliticasPrivacidade.js
@@ -6,11 +6,11 @@ function PoliticasPrivacidade() {
     <div className={styles.politicas__Container}>
         <h1 className="entry-title main_title">Política de Privacidade</h1>
         <div className="entry-content">
-            <p>A sua privacidade é importante para nós. É política do L&amp;SA.COM.BR respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site <a href="https://www.lsausa.com.br">L&amp;SA.COM.BR</a>, e outros sites que possuímos e operamos.</p>
+            <p>A sua privacidade é importante para nós. É política do L&amp;SA.COM.BR respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site <a href="https://www.lsausa.com.br" rel="noopener noreferrer">L&amp;SA.COM.BR</a>, e outros sites que possuímos e operamos.</p>
             <p>Solicitamos informações pessoais apenas quando realmente precisamos delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e consentimento. Também informamos por que estamos coletando e como será usado.</p>
             <p>Apenas retemos as informações coletadas pelo tempo necessário para fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente aceitáveis &ZeroWidthSpace;&ZeroWidthSpace;para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não autorizados.</p>
             <p>Não compartilhamos informações de identificação pessoal publicamente ou com terceiros, exceto quando exigido por lei.</p>
-            <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas <a href="https://politicaprivacidade.com" target="_BLANK" rel="noopener">políticas de privacidade</a>.</p>
+            <p>O nosso site pode ter links para sites externos que não são operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e não podemos aceitar responsabilidade por suas respectivas <a href="https://politicaprivacidade.com" target="_blank" rel="noopener noreferrer">políticas de privacidade</a>.</p>
             <p>Você é livre para recusar a nossa solicitação de informações pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.</p>
             <p>O uso continuado de nosso site será considerado como aceitação de nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.</p>
             <h2>Política de Cookies L&amp;SA.COM.BR</h2>
