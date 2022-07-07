a813374143efc5750096f697bfab49d7966715dc
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-07-07 19:43:40 -0300

    adding react toast

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { Toaster } from 'react-hot-toast';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Toaster position="bottom-right" />
+      <Component {...pageProps} />
+    </>
+    
+  )
 }
 
 export default MyApp
