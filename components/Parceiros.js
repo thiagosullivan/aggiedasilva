4b2ae0e21483746c4eb86adad3acf406e884e27f
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-07-06 23:53:19 -0300

    parceiros section

---
+++ components/Parceiros.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Hm from '../assets/hm-logo.png';
+import Ledvance from '../assets/ledvance-logo.png';
+import Lumini from '../assets/lumini-logo.png';
+import Neoris from '../assets/neoris-logo.png';
+import Niquan from '../assets/niquan-logo.png';
+import Panam from '../assets/panam-logo.png';
+import Sap from '../assets/sap-logo.png';
+import Sony from '../assets/sony-logo.png';
+import Vbmc from '../assets/vbmc-logo.png';
+
+import styles from '../styles/Home.module.css';
+
+function Parceiros() {
+  return (
+    <div className={styles.parceiros__section}>
+      <div className={styles.parceiros__content}>
+        <h2>Será um prazer ter você conosco</h2>
+        <p>Alguns dos nossos parceiros</p>
+        <div className={styles.parceiros__images}>
+          <div>
+            <Link href="https://www2.hm.com/en_us/index.html" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <Image src={Hm}/>
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="https://www.ledvance.com.br/" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <Image src={Ledvance}/>
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="https://lojavirtual.lumini.com.br/" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <Image src={Lumini}/>
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="https://www.neoris.com/" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <Image src={Neoris}/>
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="https://www.niquanenergy.com/" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <Image src={Niquan}/>
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="https://www.panam.org/" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <Image src={Panam}/>
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="https://www.sap.com/brazil/index.html" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <Image src={Sap}/>
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="https://www.sony.com.br" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <Image src={Sony}/>
+              </a>
+            </Link>
+          </div>
+          <div>
+            <Link href="https://vbmc.com.br/" passHref>
+              <a target="_blank" rel="noopener noreferrer">
+                <Image src={Vbmc}/>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Parceiros
