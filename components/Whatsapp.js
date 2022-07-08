0378ce3500235682aa009dfab4bfbdc3592b6fdf
Thiago Sullivan <thiagosullivan12@gmail.com>
2022-07-08 17:27:08 -0300

    whatsapp link

--- components/Whatsapp.js
+++ components/Whatsapp.js
@@ -7,7 +7,7 @@ import styles from '../styles/Home.module.css';
 function Whatsapp() {
   return (
     <div className={styles.whatsapp}>
-        <Link href="/" passHref>
+        <Link href="https://api.whatsapp.com/send?phone=13057535265&text=Ol%C3%A1%2C%20Aggie.%20Vim%20atrav%C3%A9s%20do%20seu%20site." passHref>
             <a target="_blank" rel="noopener noreferrer">
                 <Image alt="Whatsapp" src={WhatsappLogo}/>
             </a>
